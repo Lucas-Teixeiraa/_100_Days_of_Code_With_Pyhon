bccf99a1b8fa66464c5834cfb966d0797bc880ed
Lucas <lteixeira507@gmail.com>
2024-03-07 18:31:44 -0400

    Day 4 completed

---
+++ Day 4/Day_4.py
@@ -0,0 +1,55 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+print("Let's Play Rock, Paper and Scissor!!")
+print("------------------------------------")
+print("------------------------------------")
+print("------------------------------------")
+
+playerChoose = input("What's your choose?\n")
+
+computerRandomChoose = [scissors, paper, rock]
+computerChoose = random.choice(computerRandomChoose)
+
+if playerChoose == 'paper' and computerChoose == scissors:
+    print(f"player choose: {paper}")
+    print(f"computer choose: {computerChoose}")
+    print("Computer Win!")
+elif playerChoose == 'rock' and computerChoose == paper:
+    print(f"player choose: {rock}")
+    print(f"computer choose: {computerChoose}")
+    print("Computer Win!")
+elif playerChoose == 'scissor' and computerChoose == rock:
+    print(f"player choose: {scissors}")
+    print(f"computer choose: {computerChoose}")
+    print("Computer Win!")
+else:
+    print("You Win!")
+
+
+
